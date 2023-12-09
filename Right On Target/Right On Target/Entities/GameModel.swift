7c10f5b015befc44d03d1a8ae0e184ac2241c240
Arthur Raff <arthur.raff@icloud.com>
2023-12-09 17:25:25 +0300

    GameModel created

---
+++ Right On Target/Right On Target/Entities/GameModel.swift
@@ -0,0 +1,14 @@
+//
+//  GameModel.swift
+//  Right On Target
+//
+//  Created by Arthur Raff on 09.12.2023.
+//
+
+import Foundation
+
+struct GameModel: Codable {
+    let score: Int?
+    let currentSecretValue: Int?
+    let isGameEnded: Bool?
+}
