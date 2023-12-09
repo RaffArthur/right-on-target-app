89ea41107c6fe32aa48231ca0839f5ba9ec9e7b6
Arthur Raff <arthur.raff@icloud.com>
2023-12-09 15:54:07 +0300

    Code refactored

--- Right On Target/Right On Target/PresentationFlow/GameScreen/View/GameViewController.swift
+++ Right On Target/Right On Target/PresentationFlow/GameScreen/View/GameViewController.swift
@@ -11,15 +11,18 @@ import UIKit
 final class GameViewController: UIViewController {
     private lazy var gameView = GameView()
     
-    private lazy var round: Int = 0
+    private lazy var round: Int = 1
     private lazy var number: Int = 0
     private lazy var points: Int = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        gameView.setCheckNumberButtonTitle(title: "НАЧАТЬ")
         gameView.delegate = self
+        
+        number = Int.random(in: 1...50)
+        
+        gameView.setIntentedNumber(number: String(number))
     }
     
     override func loadView() {
@@ -28,51 +31,42 @@ final class GameViewController: UIViewController {
 }
 
 extension GameViewController: GameViewDelegate {
-    func intendedNumberWasChecked() {
-        gameView.setCheckNumberButtonTitle(title: "Проверить")
-
-        if round == 0 {
-            number = Int.random(in: 1...50)
+    func intendedNumberWasChecked() {        
+        let numSlider = gameView.getNumberPositionOnSliderValue()
+        
+        if numSlider > number {
+            points += 50 - numSlider + number
+        } else if numSlider < number {
+            points += 50 - number + numSlider
+        } else {
+            points += 50
+        }
+        
+        if round == 5 {
+            let alert = UIAlertController(title: "Игра окончена",
+                                          message: "Заработано очков: \(points)",
+                                          preferredStyle: .alert)
             
-            gameView.setIntentedNumber(number: String(number))
+            let action = UIAlertAction(title: "Начать заново",
+                                       style: .default) { [weak self] _ in
+                self?.gameView.setCheckNumberButtonTitle(title: "Проверить")
+            }
+            
+            alert.addAction(action)
+            
+            present(alert, animated: true, completion: nil)
             
             round = 1
+            
+            points = 0
+            
+            gameView.setCheckNumberButtonTitle(title: "Завершено")
         } else {
-            let numSlider = gameView.getNumberPositionValue()
-            
-            if numSlider > number {
-                points += 50 - numSlider + number
-            } else if numSlider < number {
-                points += 50 - number + numSlider
-            } else {
-                points += 50
-            }
-            
-            if round == 5 {
-                let alert = UIAlertController(title: "Игра окончена",
-                                              message: "Заработано очков: \(points)",
-                                              preferredStyle: .alert)
-                
-                let action = UIAlertAction(title: "Начать заново", style: .default) { [weak self] _ in
-                    self?.gameView.setCheckNumberButtonTitle(title: "Проверить")
-                }
-                
-                alert.addAction(action)
-                
-                present(alert, animated: true, completion: nil)
-                
-                round = 0
-                
-                points = 0
-                
-                gameView.setCheckNumberButtonTitle(title: "НАЧАТЬ")
-            } else {
-                round += 1
-            }
-            
-            number = Int.random(in: 1...50)
-            
-            gameView.setIntentedNumber(number: String(number))
+            round += 1
         }
+        
+        number = Int.random(in: 1...50)
+        
+        gameView.setIntentedNumber(number: String(number))
     }
 }
